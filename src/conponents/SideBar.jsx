286d0b39a8cc75baf6ddcd05e7eeeaf8eaa89159
Atul <dr6217811@gmail.com>
2025-07-30 23:12:50 +0530

    developing chat container

--- src/conponents/SideBar.jsx
+++ src/conponents/SideBar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import assets from "../assets/assets";
+import assets, { userDummyData } from "../assets/assets";
 
 
 
@@ -8,7 +8,7 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
     const navigate = useNavigate()
 
     return (
-    <div className={`bg-[#8185B2]/10 h-[500px] p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ""}`} >
+    <div className={`bg-[#8185B2]/10 h-[500px] p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : "" } `} >
         <div className="pb-5  " >
             <div className="  flex justify-between items-center " >
                 <img src={assets.logo} alt="logo" className=" max-w-40 " />
@@ -27,6 +27,22 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
                 <input type="text" placeholder="Search User..." className=" bg-transparent border-none outline-none text-white text-xs flex-1 placeholder-[#c8c8c8] " />
             </div>
         </div>
+        
+        <div className="flex flex-col" >
+            { userDummyData.map((user, index) => (
+                <div onClick={() => setSelectedUser(user)} key={index} className={` relative flex flex-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[282142]/50' } `} >
+                    <img src={user?.profilePic || assets.avatar_icon} alt="" className=" w-[35px] aspect-[1/1] rounded-full " />
+                    <div className="flex flex-col" >
+                        <p> {user.fullName} </p>
+                        {
+                            index < 3 ? <span className="text-green-400 text-xs" >Online</span> : <span className="text-neutral-400 text-xs" >Offline</span>
+                        }
+                    </div>
+                    { index > 2 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50 " > {index} </p> }
+                </div>
+            )) }
+        </div>
+
     </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,9 +9,9 @@ const HomePage = () => {
 
     return <div className=" border w-full h-screen sm:px-[15%] sm:py-[5%]" >
             <div className= {`text-white backdrop-blur-xl rounded-2xl overflow-hidden h-[100%] relative border-2 border-gray-600 grid grid-cols-1 ${selectedUser ? " md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr] " : " md:grid-cols-2 "} `} >
-                <SideBar />
-                <ChatContainer />
-                <RightSideBar />
+                <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
             </div>
     </div>
 }
