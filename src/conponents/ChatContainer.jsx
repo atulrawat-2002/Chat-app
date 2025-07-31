27418f21e678cb13642ccfe07d60e1dc8ce8d841
Atul <dr6217811@gmail.com>
2025-07-31 15:48:05 +0530

    chat container completed

--- src/conponents/ChatContainer.jsx
+++ src/conponents/ChatContainer.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import assets, { messagesDummyData } from "../assets/assets";
+import { formateMessageTime } from "../lib/utils";
 
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
@@ -23,23 +24,34 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
         <img src={assets.help_icon} alt="help_icon" className=" max-md:hidden max-w-5  " />
     </div>
     {/* ------------- chat area -------------- */}
-    <div className=" flex flex-col h-[calc(100%-120px)] overflow-y-scroll py-3 pb-6 " >
+    <div className="border-r border-l flex flex-col h-[calc(100%-120px)] overflow-y-scroll py-3 pb-6 " >
         { messagesDummyData.map((msg, index) => (<div key={index} className={` flex items-end gap-2 justify-end  ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse' } `}  >
-            { msg.img ? (
-                <img src={msg?.img} alt="" className=" max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8 " />
+            { msg.image ? (
+                <img src={msg?.image} alt="" className=" max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8 " />
             ) : <p className={` p-2 max-w-[200px] md:text-sm font-light bg-violet-500/30 text-white break-all mb-8 ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none' }  `} >
                 {msg.text}
             </p> }
             <div className=" text-center text-xs " >
                 <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} className="w-7 rounded-full" alt="" />
-                <p className="text-gray-500" >
-                    {msg.createdAt}
+                <p className="text-gray-300" >
+                    {formateMessageTime(msg.createdAt)}
                 </p>
             </div>
         </div>)) }
         <div ref={scrollEnd} >
-
+            
         </div>
+    </div>
+    {/* --------------- bottom area ------------------ */}
+    <div className=" absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3" >
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full" >
+            <input className="flex-1 text-sm p-3 border-none outline-none rounded-lg text-white placeholder-gray-400" type="text"  id="" placeholder="send a message" /> 
+        <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+        <label htmlFor="image">
+            <img src={assets.gallery_icon}  alt=""  className="w-5 mr-2 cursor-pointer" />
+        </label>
+        </div>
+        <img src={assets.send_button} className="cursor-pointer w-7" alt="" />
     </div>
     </div> ) : (
         <div className=" flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden " >
--- src/conponents/RightSideBar.jsx
+++ src/conponents/RightSideBar.jsx
@@ -1,6 +1,16 @@
-const RightSideBar = () => {
-    return <div>
-        <h1>Right side bar</h1>
+import assets from "../assets/assets";
+
+
+const RightSideBar = ({ selectedUser }) => {
+    return selectedUser && <div>
+        <div className="pt-16 flex flex-col items-center gap-2 font-light mx-auto text-xs" >
+            <img src={selectedUser?.profliPc || assets.avatar_icon} alt="" className="w-20 rounded-full aspect-[1/1]"/>
+            <h1 className="text-xl flex items-center px-10 font-medium  mx-auto gap-2" >
+                <p className="w-2 h-2 rounded-full bg-green-500" ></p>
+                {selectedUser.fullName}
+            </h1>
+            <p className="px-20 mx-auto" >{selectedUser.bio}</p>
+        </div>
     </div>
 }
 
